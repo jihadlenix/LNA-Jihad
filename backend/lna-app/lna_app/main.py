88e5f01cd9c6cef369ed756a50960cc5a9241bf3
jihadlenix <jihadismasarani@gmail.com>
2025-03-12 01:07:36 +0200

    added local

--- backend/lna-app/lna_app/main.py
+++ backend/lna-app/lna_app/main.py
@@ -1,20 +1,31 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
-
-from .api.news import router as news_router  # Adjust this import path based on your project structure
+from .api.news import router as news_router  # Ensure this path is correct
 
 app = FastAPI(title="Simple LNA API")
 
-app.include_router(news_router, prefix="/news")  # Add a prefix if desired, or leave it empty
+# Correcting CORS settings to be included in the same FastAPI app instance
+origins = [
+    "http://localhost:5173",  # Local frontend address
+    "https://lnafrontend.vercel.app", # Deployed frontend address]
+     "http://127.0.0.1:5173" 
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://lnafrontend.vercel.app"],  # This is for development only; specify your frontend's host for production
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(news_router, prefix="/news")  # Include your router with the prefix
+
 @app.get("/")
 async def root():
-    return {"message": "Hi"}
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
