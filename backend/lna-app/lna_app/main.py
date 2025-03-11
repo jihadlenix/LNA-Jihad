0eb6755414bb2996ae327b7d7062eeab3ac34e2a
jihadlenix <jihadismasarani@gmail.com>
2025-03-11 19:22:54 +0200

    added deployed frontend origin

--- backend/lna-app/lna_app/main.py
+++ backend/lna-app/lna_app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+from .api.news import router as news_router  # Adjust this import path based on your project structure
 
 app = FastAPI(title="Simple LNA API")
 
+app.include_router(news_router, prefix="/news")  # Add a prefix if desired, or leave it empty
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://lnafrontend.vercel.app"],  # This is for development only; specify your frontend's host for production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def root():
     return {"message": "Hi"}
